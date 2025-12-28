cf5a9a381c4c365eb30e970b1c856ea22628a313
sane4ek8 <sanyard8@gmail.com>
2025-12-28 23:01:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-DATA_FILE = "storage.json"
 PEOPLE_STORAGE = "people_storage.json"
-MAN_FILE = "managers.json"
+TAXI_STORAGE = "storage.json"
+MANAGERS_FILE = "managers.json"
 
 MAX_IN_CAR = 4
